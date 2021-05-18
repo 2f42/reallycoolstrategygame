6f7f52172379080bd7b56fa19461107fa2c6342d
baconsenpaii <baconforevery2@gmail.com>
2021-05-18 02:37:42 +0100

    added really basic game loop

---
+++ game/include/gamestate.h
@@ -0,0 +1,9 @@
+#ifndef __GAMESTATE_H_
+#define __GAMESTATE_H_
+
+typedef struct gamestate {
+    int running;
+    int steptime;
+} gamestate_t;
+
+#endif
--- game/main.c
+++ game/main.c
@@ -1,3 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "gamestate.h"
+
+int initialise (gamestate_t *game);
+int gameloop (gamestate_t *game);
+void cleanup (gamestate_t *game);
+
 int main (int argc, char **argv) {
+    gamestate_t game;
+
+    int initfail = initialise(&game);
+    if (initfail) {
+        return initfail;
+    }
+
+    int gamefail = gameloop(&game);
+    cleanup(&game);
+
+    return gamefail;
+}
+
+int initialise (gamestate_t *game) {
+    game = malloc(sizeof(gamestate_t));
+
+    if (!game) {
+        return 1;
+    }
+
+    game->running = 1;
+    game->steptime = 1000; // once per second
+
     return 0;
+}
+
+int gameloop (gamestate_t *game) {
+    while (game->running) {
+        // process inputs
+        // process commands
+
+        // if enough time has passed
+            // step the game state
+
+        // render
+    }
+
+    return 0;
+}
+
+void cleanup (gamestate_t *game) {
+    free(game);
 }
