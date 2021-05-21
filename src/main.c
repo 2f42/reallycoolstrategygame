4a7508c79d25d3fe0577cd3fcf74762acd1aa035
baconsenpaii <baconforevery2@gmail.com>
2021-05-21 06:11:00 +0100

    moved stuff out of main.c to game.c

--- src/main.c
+++ src/main.c
@@ -1,91 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 #include "timing.h"
 #include "random.h"
-#include "gamestate.h"
+#include "game.h"
 #include "command.h"
 
 
 #define SEED (0x0213de4208353f2eUL) // my lucky number
 
 
-gamestate_t *newstate (void);
-int gameloop (void);
-void cleanup (void);
-
-
-static gamestate_t *game;
-
-
 int main (int argc, char **argv) {
     random_seed(SEED);
 
-    game = newstate();
-    if (!game) {
+    gamestate_t **game;
+    game = getgamestate();
+    *game = newstate();
+    if (!*game) {
         return -1;
     }
 
     int gamefail = gameloop();
-    cleanup();
+    freestate();
 
     return gamefail;
-}
-
-gamestate_t *getgamestate (void) {
-    return game;
-}
-
-gamestate_t *newstate (void) {
-    gamestate_t *newgame = malloc(sizeof(gamestate_t));
-
-    if (!newgame) {
-        return 0;
-    }
-
-    newgame->running = 1;
-    newgame->steptime = 1000; // once per second
-
-    newgame->commands = newcommandqueue();
-
-    return newgame;
-}
-
-static void printerlmao (command_t *command) {
-    printf("%d\n", command->instr);
-
-    if (command->instr == 0) {
-        game->running = 0;
-    }
-}
-
-int gameloop (void) {
-    long start = millis();
-    long now;
-
-    while (game->running) {
-        // process inputs
-        // process commands
-
-        processcommands(game->commands, &printerlmao);
-
-        // if enough time has passed
-            // step the game state
-
-        now = millis();
-        if (now - start >= game->steptime) {
-            command_t *newcommand = makecommand((int) ranl() & 7);
-            enqueuecommand(game->commands, newcommand);
-            start = now;
-        }
-
-        // render
-    }
-
-    return 0;
-}
-
-void cleanup (void) {
-    freecommandqueue(game->commands);
-    free(game);
 }
