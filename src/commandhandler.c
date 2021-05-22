25545dd6e7488fe75c86c1174aebcbef52bf448c
baconsenpaii <baconforevery2@gmail.com>
2021-05-22 03:52:03 +0100

    used game state operations in the commandhandler

--- src/commandhandler.c
+++ src/commandhandler.c
@@ -6,9 +6,17 @@
 void game_commandhandler (command_t *command) {  
 	printf("%d\n", command->instr);
 
+	gamestate_t **state = getgamestate();
+
 	switch (command->instr) {
 		case CMD_END_GAME:
-			(*getgamestate())->running = 0;
+			game_stop();
+			return;
+		case CMD_PAUSE_GAME:
+			game_pause();
+			return;
+		case CMD_UNPAUSE_GAME:
+			game_unpause();
 			return;
 		default:
 			return;
